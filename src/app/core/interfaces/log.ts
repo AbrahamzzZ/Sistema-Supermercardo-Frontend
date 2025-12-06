7e4c6e454073deb88423a864cf1ce24f9c8479fe
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-12-06 15:05:56 -0500

    Se modifico el nombre de las propiedades para mostrar la informacion del log en la tabla correctamente

--- src/app/core/interfaces/log.ts
+++ src/app/core/interfaces/log.ts
@@ -1,9 +1,9 @@
 export interface ILog {
-    id_log: number;
-    codigo_error: string;
-    mensaje_error: string;
-    detalle_error: string;
-    id_usuario?: number;
+    id_Log: number;
+    codigo_Error: string;
+    mensaje_Error: string;
+    detalle_Error: string;
+    id_Usuario?: number;
     fecha: string;
     endpoint: string;
     metodo: string;
