7819772a5e4279622d50df0762516a0570f52255
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-02-21 23:46:19 -0500

    Se configuro para que cargue el mapa en el componente

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -6,12 +6,30 @@ import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@a
 import { AuthInterceptor } from './core/interceptor/auth.interceptor';
 import { ErrorInterceptor } from './core/interceptor/error.interceptor';
 import { LoaderInterceptor } from './core/interceptor/loader.interceptor';
+import { setOptions, importLibrary } from '@googlemaps/js-api-loader';
+import { environment as ENV } from '../../environments/environment';
+
+function loadGoogleMaps() {
+  return async () => {
+    try {
+      setOptions({
+        key: ENV.API_GOOGLE_MAPS
+      });
+      await importLibrary('maps');
+    } catch (error: unknown) {
+      console.error('Error cargando Google Maps', error);
+      throw error;
+    }
+  };
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(withInterceptorsFromDi()),{
+    provideHttpClient(withInterceptorsFromDi()),
+
+    {
       provide: HTTP_INTERCEPTORS,
       useClass: LoaderInterceptor,
       multi: true
@@ -24,6 +42,12 @@ export const appConfig: ApplicationConfig = {
     {
       provide: HTTP_INTERCEPTORS,
       useClass: ErrorInterceptor,
+      multi: true
+    },
+
+    {
+      provide: APP_INITIALIZER,
+      useFactory: loadGoogleMaps,
       multi: true
     }
   ]
