7ccc65a7ef98d7a04eda9bea9207312a818dd59a
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-08-25 23:53:45 -0500

    feat: se creo un nuevo componente reutilizable

---
+++ src/app/shared/utility/components/data-table/data-table.component.ts
@@ -0,0 +1,68 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { TableColumn } from '../tableColumn';
+import { MaterialModule } from '../../../ui/material-module';
+
+@Component({
+  selector: 'app-data-table',
+  standalone: true,
+  imports: [MaterialModule],
+  templateUrl: './data-table.component.html',
+  styleUrl: './data-table.component.scss'
+})
+export class DataTableComponent {
+  @Input() dataSource = new MatTableDataSource<any>();
+
+  @Input() columns: TableColumn[] = [];
+
+  @Input() totalRegistros = 0;
+
+  @Input() pageSize = 5;
+
+  @Input() pageSizeOptions: number[] = [5, 10, 20];
+
+  @Input() mensajeSinDatos = 'No hay datos que mostrar.';
+
+  @Output() editar = new EventEmitter<any>();
+
+  @Output() eliminar = new EventEmitter<any>();
+
+  @Output() ver = new EventEmitter<any>();
+
+  @Output() cambioPagina = new EventEmitter<PageEvent>();
+
+  get displayedColumns(): string[] {
+    return this.columns.map(column => column.key);
+  }
+
+  getFechaRegistro(fecha: string): string {
+    const fechaObj = new Date(fecha);
+
+    return fechaObj.toLocaleDateString('es-ES', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric'
+    });
+  }
+
+  getEstado(estado: boolean): string {
+    return estado ? 'Activo' : 'Inactivo';
+  }
+
+  onEditar(element: any): void {
+    this.editar.emit(element);
+  }
+
+  onEliminar(element: any): void {
+    this.eliminar.emit(element);
+  }
+
+  onVer(element: any): void {
+    this.ver.emit(element);
+  }
+
+  onPageChange(event: PageEvent): void {
+    this.cambioPagina.emit(event);
+  }
+}
