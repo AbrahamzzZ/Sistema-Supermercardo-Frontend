6db2edae20e80ba9b5b1ecb8d8322843aac986da
AbrahamzzZ <hermanosfarfan@gmail.com>
2026-09-19 13:14:54 -0500

    fix: se configuro las variables de entorno por ambiente

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'API_URL',
-  API_GOOGLE_MAPS: 'TU_API_KEY_DE_PRODUCCION'
+  API_URL: 'TU_API_URL_PRODUCCION',
+  API_GOOGLE_MAPS: 'TU_API_KEY_PRODUCCION'
 };
