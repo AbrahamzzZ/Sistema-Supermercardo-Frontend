433beade7a6900e8372d0dd96cec0d42cc9f5402
Abraham Farfan <hermanosfarfan@gmail.com>
2026-09-23 16:21:47 -0500

    fix: se agrego el nuevo componente de los formularios al ruteo de la aplicacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,36 +6,28 @@ import { PaginaNoEncontradaComponent } from './presentation/pages/pagina-no-enco
 import { TransportistaInicioComponent } from './presentation/pages/transportista-inicio/transportista-inicio.component';
 import { ProveedorInicioComponent } from './presentation/pages/proveedor-inicio/proveedor-inicio.component';
 import { ProductoInicioComponent } from './presentation/pages/producto-inicio/producto-inicio.component';
-import { RegistroProductoComponent } from './presentation/pages/producto-inicio/registro-producto/registro-producto.component';
-import { RegistroClienteComponent } from './presentation/pages/cliente-inicio/registro-cliente/registro-cliente.component';
-import { RegistroUsuarioComponent } from './presentation/pages/usuario-inicio/registro-usuario/registro-usuario.component';
-import { RegistroTransportistaComponent } from './presentation/pages/transportista-inicio/registro-transportista/registro-transportista.component';
-import { RegistroProveedorComponent } from './presentation/pages/proveedor-inicio/registro-proveedor/registro-proveedor.component';
-import { ProductoEditarComponent } from './presentation/pages/producto-inicio/producto-editar/producto-editar.component';
-import { EditarProveedorComponent } from './presentation/pages/proveedor-inicio/editar-proveedor/editar-proveedor.component';
-import { EditarTransportistaComponent } from './presentation/pages/transportista-inicio/editar-transportista/editar-transportista.component';
-import { EditarUsuarioComponent } from './presentation/pages/usuario-inicio/editar-usuario/editar-usuario.component';
-import { EditarClienteComponent } from './presentation/pages/cliente-inicio/editar-cliente/editar-cliente.component';
+import { FormularioProductoComponent } from './presentation/pages/producto-inicio/formulario-producto/formulario-producto.component';
+import { FormularioClienteComponent } from './presentation/pages/cliente-inicio/formulario-cliente/formulario-cliente.component';
+import { FormularioUsuarioComponent } from './presentation/pages/usuario-inicio/formulario-usuario/formulario-usuario.component';
+import { FormularioTransportistaComponent } from './presentation/pages/transportista-inicio/formulario-transportista/formulario-transportista.component';
+import { FormularioProveedorComponent } from './presentation/pages/proveedor-inicio/formulario-proveedor/formulario-proveedor.component';
 import { FormularioIncompleto } from './core/guards/formulario-incompleto.guard';
 import { OfertaInicioComponent } from './presentation/pages/oferta-inicio/oferta-inicio.component';
-import { RegistroOfertaComponent } from './presentation/pages/oferta-inicio/registro-oferta/registro-oferta.component';
-import { EditarOfertaComponent } from './presentation/pages/oferta-inicio/editar-oferta/editar-oferta.component';
+import { FormularioOfertaComponent } from './presentation/pages/oferta-inicio/formulario-oferta/formulario-oferta.component';
 import { LoginLayoutComponent } from './presentation/components/layouts/login-layout/login-layout.component';
 import { LoginComponent } from './presentation/pages/login/login.component';
 import { MainLayoutComponent } from './presentation/components/layouts/main-layout/main-layout.component';
 import { Autenticacion } from './core/guards/autenticacion.guard';
 import { RolGuard } from './core/guards/rol.guard';
 import { CategoriaInicioComponent } from './presentation/pages/categoria-inicio/categoria-inicio.component';
-import { RegistroCategoriaComponent } from './presentation/pages/categoria-inicio/registro-categoria/registro-categoria.component';
-import { EditarCategoriaComponent } from './presentation/pages/categoria-inicio/editar-categoria/editar-categoria.component';
+import { FormularioCategoriaComponent } from './presentation/pages/categoria-inicio/formulario-categoria/formulario-categoria.component';
 import { CompraInicioComponent } from './presentation/pages/compra-inicio/compra-inicio.component';
 import { VentaInicioComponent } from './presentation/pages/venta-inicio/venta-inicio.component';
 import { DetalleCompraComponent } from './presentation/pages/compra-inicio/detalle-compra/detalle-compra.component';
 import { DetalleVentaComponent } from './presentation/pages/venta-inicio/detalle-venta/detalle-venta.component';
 import { NegocioInicioComponent } from './presentation/pages/negocio-inicio/negocio-inicio.component';
 import { SucursalInicioComponent } from './presentation/pages/sucursal-inicio/sucursal-inicio.component';
-import { RegistrarSucursalComponent } from './presentation/pages/sucursal-inicio/registrar-sucursal/registrar-sucursal.component';
-import { EditarSucursalComponent } from './presentation/pages/sucursal-inicio/editar-sucursal/editar-sucursal.component';
+import { FormularioSucursalComponent } from './presentation/pages/sucursal-inicio/formulario-sucursal/formulario-sucursal.component';
 import { EstadisticaNegocioComponent } from './presentation/pages/negocio-inicio/estadistica-negocio/estadistica-negocio.component';
 import { LogInicioComponent } from './presentation/pages/log-inicio/log-inicio.component';
 import { MapaSucursalComponent } from './presentation/pages/sucursal-inicio/mapa-sucursal/mapa-sucursal.component';
@@ -64,16 +56,17 @@ export const routes: Routes = [
         title: 'Usuarios'
       },
       {
-        path: 'usuario/usuario-registro/:id',
+        path: 'usuario/registro',
         canActivate: [Autenticacion],
         canDeactivate: [FormularioIncompleto],
-        component: RegistroUsuarioComponent,
+        component: FormularioUsuarioComponent,
         title: 'Registro de Usuarios'
       },
       {
-        path: 'usuario/usuario-editar/:id',
+        path: 'usuario/editar/:id',
         canActivate: [Autenticacion],
-        component: EditarUsuarioComponent,
+        canDeactivate: [FormularioIncompleto],
+        component: FormularioUsuarioComponent,
         title: 'Editar Usuario'
       },
 
@@ -86,16 +79,17 @@ export const routes: Routes = [
         title: 'Clientes'
       },
       {
-        path: 'cliente/cliente-registro/:id',
+        path: 'cliente/registro',
         canActivate: [Autenticacion],
         canDeactivate: [FormularioIncompleto],
-        component: RegistroClienteComponent,
+        component: FormularioClienteComponent,
         title: 'Registro de Clientes'
       },
       {
-        path: 'cliente/cliente-editar/:id',
+        path: 'cliente/editar/:id',
         canActivate: [Autenticacion],
-        component: EditarClienteComponent,
+        canDeactivate: [FormularioIncompleto],
+        component: FormularioClienteComponent,
         title: 'Editar Cliente'
       },
 
@@ -108,16 +102,17 @@ export const routes: Routes = [
         title: 'Transportistas'
       },
       {
-        path: 'transportista/transportista-registro/:id',
+        path: 'transportista/registro',
         canActivate: [Autenticacion],
         canDeactivate: [FormularioIncompleto],
-        component: RegistroTransportistaComponent,
+        component: FormularioTransportistaComponent,
         title: 'Registro de Transportistas'
       },
       {
-        path: 'transportista/transportista-editar/:id',
+        path: 'transportista/editar/:id',
         canActivate: [Autenticacion],
-        component: EditarTransportistaComponent,
+        canDeactivate: [FormularioIncompleto],
+        component: FormularioTransportistaComponent,
         title: 'Editar Transportista'
       },
 
@@ -130,16 +125,17 @@ export const routes: Routes = [
         title: 'Proveedor'
       },
       {
-        path: 'proveedor/proveedor-registro/:id',
+        path: 'proveedor/registro',
         canActivate: [Autenticacion],
         canDeactivate: [FormularioIncompleto],
-        component: RegistroProveedorComponent,
+        component: FormularioProveedorComponent,
         title: 'Registro de Proveedores'
       },
       {
-        path: 'proveedor/proveedor-editar/:id',
+        path: 'proveedor/editar/:id',
         canActivate: [Autenticacion],
-        component: EditarProveedorComponent,
+        canDeactivate: [FormularioIncompleto],
+        component: FormularioProveedorComponent,
         title: 'Editar Proveedor'
       },
 
@@ -151,16 +147,17 @@ export const routes: Routes = [
         title: 'Categoria'
       },
       {
-        path: 'categoria/categoria-registro/:id',
+        path: 'categoria/registro',
         canActivate: [Autenticacion],
         canDeactivate: [FormularioIncompleto],
-        component: RegistroCategoriaComponent,
+        component: FormularioCategoriaComponent,
         title: 'Registro de Categorías'
       },
       {
-        path: 'categoria/categoria-editar/:id',
+        path: 'categoria/editar/:id',
         canActivate: [Autenticacion],
-        component: EditarCategoriaComponent,
+        canDeactivate: [FormularioIncompleto],
+        component: FormularioCategoriaComponent,
         title: 'Editar Categoría'
       },
 
@@ -172,16 +169,17 @@ export const routes: Routes = [
         title: 'Producto'
       },
       {
-        path: 'producto/producto-registro/:id',
+        path: 'producto/registro',
         canActivate: [Autenticacion],
         canDeactivate: [FormularioIncompleto],
-        component: RegistroProductoComponent,
+        component: FormularioProductoComponent,
         title: 'Registro de Productos'
       },
       {
-        path: 'producto/producto-editar/:id',
+        path: 'producto/editar/:id',
         canActivate: [Autenticacion],
-        component: ProductoEditarComponent,
+        canDeactivate: [FormularioIncompleto],
+        component: FormularioProductoComponent,
         title: 'Editar Producto'
       },
 
@@ -193,16 +191,17 @@ export const routes: Routes = [
         title: 'Oferta'
       },
       {
-        path: 'oferta/oferta-registro/:id',
+        path: 'oferta/registro',
         canActivate: [Autenticacion],
         canDeactivate: [FormularioIncompleto],
-        component: RegistroOfertaComponent,
+        component: FormularioOfertaComponent,
         title: 'Registro de Ofertas'
       },
       {
-        path: 'oferta/oferta-editar/:id',
+        path: 'oferta/editar/:id',
         canActivate: [Autenticacion],
-        component: EditarOfertaComponent,
+        canDeactivate: [FormularioIncompleto],
+        component: FormularioOfertaComponent,
         title: 'Editar Oferta'
       },
 
@@ -243,16 +242,17 @@ export const routes: Routes = [
         title: 'Sucursales nacionales'
       },
       {
-        path: 'sucursal/sucursal-registro/:id',
-        component: RegistrarSucursalComponent,
+        path: 'sucursal/registro',
+        component: FormularioSucursalComponent,
         canActivate: [Autenticacion],
         canDeactivate: [FormularioIncompleto],
         title: 'Registar Sucursal'
       },
       {
-        path: 'sucursal/sucursal-editar/:id',
-        component: EditarSucursalComponent,
+        path: 'sucursal/editar/:id',
+        component: FormularioSucursalComponent,
         canActivate: [Autenticacion],
+        canDeactivate: [FormularioIncompleto],
         title: 'Editar Sucursal'
       },
       {
