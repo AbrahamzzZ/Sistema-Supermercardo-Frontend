77ff4cbc3990af51e073450944d6ee23753dbae4
Abraham Farfan <hermanosfarfan@gmail.com>
2026-09-25 14:50:38 -0500

    fix: se mejoraron los interceptores y se elimino el de timeout

--- src/app/core/interceptor/auth.interceptor.ts
+++ src/app/core/interceptor/auth.interceptor.ts
@@ -1,9 +1,10 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { CLAVES_STORAGE } from '../constants/storage.const';
 
 export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem(CLAVES_STORAGE.token);
 
     if (req.url.includes('/login')) {
       return next.handle(req);
